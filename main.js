74a3e7f91c28f435161223ea93c9526b0679e7c3
nadocodes <naderantar96@gmail.com>
2023-02-05 16:40:55 +0000

    made main.js more readable

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const prompt = require('prompt-sync')({sigint: true});
-var colors = require('colors');
+const prompt = require("prompt-sync")({ sigint: true });
+var colors = require("colors");
 
 colors.enable();
 
-const hat = '^'.green;
-const hole = 'O'.red;
-const fieldCharacter = '░'.blue;
-const pathCharacter = '*'.magenta;
+const hat = "^".green;
+const hole = "O".red;
+const fieldCharacter = "░".blue;
+const pathCharacter = "*".magenta;
 
 class Field {
   constructor(field) {
@@ -19,44 +19,46 @@ class Field {
     while (playing) {
       this.print();
       let move = prompt('Which way? "u=↑, r=→, d=↓, l=←" ').toUpperCase();
-      if (move === 'U') {
+      if (move === "U") {
         y--; // move up one row
-        } else if (move === 'D') {
+      } else if (move === "D") {
         y++; // move down one row
-        }
-        else if (move === 'L') {
+      } else if (move === "L") {
         x--; // move left one column
-        }
-        else if (move === 'R') {
+      } else if (move === "R") {
         x++; // move right one column
-        }
-        else {
-        console.log('Invalid move!');
-        }
-        if (y < 0 || y >= this.field.length || x < 0 || x >= this.field[0].length) {
-          console.log('YOU ARE OUTSIDE!'); // went outside the field
-          playing = false;
-        }
-        else if (this.field[y][x] === hole) {  // fell down a hole
-        console.log('Game Over! You fell down a hole!');
-        playing = false; // game over
-        }
-        else if (this.field[y][x] === hat) {  // found the hat
-        console.log('You Win!');
-        playing = false; // game over
-        }
-        else {
-        this.field[y][x] = pathCharacter; // show character on field
-        }
-
+      } else {
+        console.log("Invalid move!");
       }
-  }
-  print() { // print the field
-    for (let i = 0; i < this.field.length; i++) {
-      console.log(this.field[i].join(''));
+      if (
+        y < 0 ||
+        y >= this.field.length ||
+        x < 0 ||
+        x >= this.field[0].length
+      ) {
+        console.log("YOU ARE OUTSIDE!"); // went outside the field
+        playing = false;
+      } else if (this.field[y][x] === hole) {
+        // fell down a hole
+        console.log("Game Over! You fell down a hole!");
+        playing = false; // game over
+      } else if (this.field[y][x] === hat) {
+        // found the hat
+        console.log("You Win!");
+        playing = false; // game over
+      } else {
+        this.field[y][x] = pathCharacter; // show character on field
+      }
     }
   }
-  static generateField(height, width, percentage) { // generate random field to play on
+  print() {
+    // print the field
+    for (let i = 0; i < this.field.length; i++) {
+      console.log(this.field[i].join(""));
+    }
+  }
+  static generateField(height, width, percentage) {
+    // generate random field to play on
     let field = []; // array to hold the field
     for (let i = 0; i < height; i++) {
       let row = []; // array to hold each row
